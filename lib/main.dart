d1b8dd8bcc1d80c9d699be317ed00e7475433b1a
izajulhaque <md.izajul@gmail.com>
2025-09-06 15:11:14 +0600

    flutter flame physics project testing initial

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'components/game.dart';
+
+void main() {
+  runApp(GameWidget.controlled(gameFactory: MyPhysicsGame.new));
+
+}
