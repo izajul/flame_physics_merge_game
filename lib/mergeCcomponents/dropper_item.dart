de74acbceb3eaa6cffaaedd6124cb29fd46fbe15
izajulhaque <md.izajul@gmail.com>
2025-09-13 19:19:58 +0600

    dropper_item.dart fruits size factor updated

--- lib/mergeCcomponents/dropper_item.dart
+++ lib/mergeCcomponents/dropper_item.dart
@@ -6,22 +6,23 @@ import 'package:flame_physics/mergeCcomponents/body_with_data_component.dart';
 import 'package:flame_physics/merge_game.dart';
 
 enum FruitItem {
-  item_1(itemSize: 2.3, fruitNumber: 1),
-  item_2(itemSize: 3.0, fruitNumber: 2),
-  item_3(itemSize: 3.7, fruitNumber: 3),
-  item_4(itemSize: 4.4, fruitNumber: 4),
-  item_5(itemSize: 5.1, fruitNumber: 5),
-  item_6(itemSize: 5.8, fruitNumber: 6),
-  item_7(itemSize: 6.5, fruitNumber: 7),
-  item_8(itemSize: 7.2, fruitNumber: 8),
-  item_9(itemSize: 7.9, fruitNumber: 9),
-  item_10(itemSize: 8.6, fruitNumber: 10),
-  item_11(itemSize: 9.3, fruitNumber: 11),
-  item_12(itemSize: 10.0, fruitNumber: 12),
-  item_13(itemSize: 10.7, fruitNumber: 13),
-  item_14(itemSize: 11.4, fruitNumber: 14),
-  item_15(itemSize: 12.1, fruitNumber: 15),
-  item_16(itemSize: 12.8, fruitNumber: 16);
+  item_1(itemSize: 2.2, fruitNumber: 1),
+  item_2(itemSize: 2.8, fruitNumber: 2),
+  item_3(itemSize: 3.4, fruitNumber: 3),
+  item_4(itemSize: 4.0, fruitNumber: 4),
+  item_5(itemSize: 4.6, fruitNumber: 5),
+  item_6(itemSize: 5.2, fruitNumber: 6),
+  item_7(itemSize: 5.8, fruitNumber: 7),
+  item_8(itemSize: 6.4, fruitNumber: 8),
+  item_9(itemSize: 7.0, fruitNumber: 9),
+  item_10(itemSize: 7.6, fruitNumber: 10),
+  item_11(itemSize: 8.3, fruitNumber: 11),
+  item_12(itemSize: 9.0, fruitNumber: 12),
+  item_13(itemSize: 9.7, fruitNumber: 13),
+  item_14(itemSize: 10.4, fruitNumber: 14),
+  item_15(itemSize: 11.1, fruitNumber: 15),
+  item_16(itemSize: 11.8, fruitNumber: 16),
+  ;
 
   final double itemSize;
   final int fruitNumber;
@@ -43,7 +44,7 @@ enum FruitItem {
   }
 
   // get random item, the random max item will be first 6 item only
-  static FruitItem get randomItem => FruitItem.values[Random().nextInt(6)];
+  static FruitItem get randomItem => FruitItem.values[Random().nextInt(5)];
 }
 
 class DropperItem extends BodyWithDataComponent<MergeGame>
